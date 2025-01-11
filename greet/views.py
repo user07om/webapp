8609f12fa3e7998c758052aaa2f6ca73ee573237
user07om <omkarbulbule7@gmail.com>
2025-01-12 00:55:56 +0530

    First INIT

---
+++ greet/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+class HelloUser(View):
+    template_name = "home.html"
+
+    def get(self, request, *args, **kwargs):
+        logger.info("initated the get reqeust!")
+        context = {
+            'name': 'john.diago',
+            'email': 'johndiago7@gmail.com'
+        }
+        return render(request, self.template_name, context)
